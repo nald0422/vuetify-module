9a3dc31dd972ec9720aa88100c788908273bc62d
nald04 <nald04.db@gmail.com>
2019-07-31 18:24:10 +0800

    Input/Forms

--- src/components/Input&Forms/Form.vue
+++ src/components/Input&Forms/Form.vue
@@ -7,15 +7,15 @@
      <hr />
      <div class="form-group">
       <label for="email">Mail</label>
-      <input type="text" id="email" class="form-control" />
+      <input type="text" id="email" class="form-control" v-model="mail" />
      </div>
      <div class="form-group">
       <label for="password">Password</label>
-      <input type="password" id="password" class="form-control" />
+      <input type="password" id="password" class="form-control" v-model="password" />
      </div>
      <div class="form-group">
       <label for="age">Age</label>
-      <input type="number" id="age" class="form-control" />
+      <input type="number" id="age" class="form-control" v-model="age" />
      </div>
     </div>
    </div>
@@ -24,7 +24,7 @@
      <label for="message">Message</label>
      <br />
      <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-     <textarea id="message" rows="5" class="form-control"></textarea>
+     <textarea id="message" rows="5" class="form-control" v-model="message"></textarea>
     </div>
    </div>
    <div class="row">
@@ -42,18 +42,19 @@
    <div class="row">
     <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
      <label for="male">
-      <input type="radio" id="male" value="Male" /> Male
+      <input type="radio" id="male" value="Male" v-model="gender" /> Male
      </label>
      <label for="female">
-      <input type="radio" id="female" value="Female" /> Female
+      <input type="radio" id="female" value="Female" v-model="gender" /> Female
      </label>
     </div>
    </div>
    <div class="row">
     <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
      <label for="priority">Priority</label>
-     <select id="priority" class="form-control">
-      <option></option>
+     <select id="priority" class="form-control" v-model="priority">
+      <option>Normal</option>
+      <option>Urgent</option>
      </select>
     </div>
    </div>
@@ -71,19 +72,19 @@
      <div class="panel-heading">
       <h4>Your Data</h4>
      </div>
-     <div class="panel-body">
-      <p>Mail:</p>
-      <p>Password:</p>
-      <p>Age:</p>
-      <p>Message:</p>
+     <div class="panel-body" style="font-size: 18px;">
+      <p>Mail: {{mail}}</p>
+      <p>Password: {{password}}</p>
+      <p>Age: {{age}}</p>
+      <p>Message: {{message}}</p>
       <p>
        <strong>Send Mail?</strong>
       </p>
       <ul>
        <li></li>
       </ul>
-      <p>Gender:</p>
-      <p>Priority:</p>
+      <p>Gender: {{gender}}</p>
+      <p>Priority: {{priority}}</p>
       <p>Switched:</p>
      </div>
     </div>
@@ -93,8 +94,19 @@
 </template>
 
 <script>
-export default {};
+export default {
+ data: function() {
+  return {
+   mail: "",
+   password: "",
+   age: "",
+   message: "",
+   gender: "",
+   priority: ""
+  };
+ }
+};
 </script>
 
-<style>
+<style scoped>
 </style>

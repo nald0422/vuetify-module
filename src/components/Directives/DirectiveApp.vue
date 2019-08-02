affa4e7b56e3013541a7ddcb69389db7f00c42bf
nald04 <nald04.db@gmail.com>
2019-08-02 18:25:01 +0800

    Local/Global Directives

--- src/components/Directives/DirectiveApp.vue
+++ src/components/Directives/DirectiveApp.vue
@@ -1,15 +1,53 @@
 <template>
  <div class="container">
   <div class="row">
-   <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"></div>
-   <p v-text="'This is Some Text'"></p>
+   <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+    <h1>Built-in Directives</h1>
+    <p v-text="'This is Some Text'"></p>
+    <p v-html="'<Strong>This is Html</Strong>'"></p>
+   </div>
+  </div>
+  <hr />
+  <div class="row">
+   <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+    <h1>Custom Directives</h1>
+    <h5>Global directives</h5>
+    <p v-highlight:background="{backgroundColor:'black', secondColor:'white'}">Highlight</p>
+    <h5>Local directives</h5>
+    <p v-highlight-local:background.delayed="'yellow'">Highlight</p>
+   </div>
   </div>
  </div>
 </template>
 
 <script>
-export default {};
+import { setTimeout } from "timers";
+export default {
+ directives: {
+  "highlight-local": {
+   bind(el, binding, vnode) {
+    // el.style.backgroundColor = 'green';
+    // el.style.backgroundColor = binding.value;
+    var delay = 0;
+    if (binding.modifiers["delayed"]) {
+     delay = 3000;
+    }
+    setTimeout(() => {
+     if (binding.arg == "background") {
+      el.style.backgroundColor = binding.value;
+      if (binding.value == "black") {
+       el.style.color = "white";
+      }
+     } else {
+      el.style.color = binding.value;
+     }
+     console.log("Delay : " + delay);
+    }, delay);
+   }
+  }
+ }
+};
 </script>
 
-<style>
+<style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -21,8 +21,25 @@ export const eventBus = new Vue({
     }
 });
 
-Vue.component('app-servers', VueDemo)
 Vue.config.productionTip = false
+
+Vue.component('app-servers', VueDemo)
+
+Vue.directive('highlight', {
+    bind(el, binding, vnode) {
+        // el.style.backgroundColor = 'green';
+        // el.style.backgroundColor = binding.value;
+        if (binding.arg == 'background') {
+            el.style.backgroundColor = binding.value.backgroundColor;
+            el.style.color = binding.value.secondColor;
+            if (binding.value == "black") {
+                el.style.color = "white";
+            }
+        } else {
+            el.style.color = binding.value;
+        }
+    }
+})
 
 new Vue({
     el: "#app",
